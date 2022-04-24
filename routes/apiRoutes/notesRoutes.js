8f85dac6d20c2c3be59d264d64058eb2e27ec793
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-04-23 21:41:01 -0500

    hopefully have back-end, some issues on front-end

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {validateNote, createNewNote} = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+router.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.post('/api/notes', (req, res) => {
+    // use uuid package to create a unique id for the new note
+    req.body.id = uuidv4();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.')
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+module.exports = router;
